0972515f0d9cea533e74274113425bffd5659b98
Ovchinnikova-mary <61615971+Ovchinnikova-mary@users.noreply.github.com>
2020-03-15 19:28:26 +0300

    Add files via upload

---
+++ lab1_Task3.lisp
@@ -0,0 +1,7 @@
+;gnu clisp 2.49 
+(defconstant L (list (list 5 6 7) 6 (list 2 3 4) (car (list 1 2 3)) (list 1 3 1 4))) 
+
+(print (nth 2 L)) 
+(print (nth 1 (nth 4 L))) 
+(print (+ (nth 1 L) (nth 3 L))) 
+(print (nth 2 (nth 0 L)))
